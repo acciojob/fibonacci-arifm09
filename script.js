a8cca609761d8f2cf300829ec1875df28378cc14
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-05 19:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,4 @@ function fibonacci(num) {
 }
 
 module.exports = fibonacci;
-cy.request({
-  method: 'POST',
-  url: 'http://localhost:3000/fibnocci',
-  timeout: 60000 // increase timeout to 60 seconds
-});
+
